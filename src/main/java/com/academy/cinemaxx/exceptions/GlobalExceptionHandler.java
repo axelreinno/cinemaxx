6279a08a80a3e12dc29086763df285fbcc793ea3
Axel Reinno Fabiyanto <axel.fabiyanto@nostratech.com>
2025-06-16 22:01:52 +0700

    fix: remove BadRequestException exception handler and adjust MethodArgumentNotValidException exception for global errors

--- src/main/java/com/academy/cinemaxx/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/academy/cinemaxx/exceptions/GlobalExceptionHandler.java
@@ -1,22 +1,18 @@
 package com.academy.cinemaxx.exceptions;
 
 import com.academy.cinemaxx.dtos.ErrorResponseDTO;
-import com.academy.cinemaxx.dtos.ResponseDTO;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
-import jakarta.validation.ValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,22 +29,21 @@ public class GlobalExceptionHandler {
                 .body(ErrorResponseDTO.error(ex.getMessage()));
     }
 
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ErrorResponseDTO<Object>> handleBadRequest(BadRequestException ex) {
-        log.warn("LOG: Bad Request: {}", ex.getMessage());
-        return ResponseEntity.badRequest().body(ErrorResponseDTO.error(ex.getMessage()));
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseDTO<Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
 
-        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
             errors.put(error.getField(), error.getDefaultMessage());
-        }
+        });
+
+        ex.getBindingResult().getGlobalErrors().forEach(error -> {
+            errors.put(error.getObjectName(), error.getDefaultMessage());
+        });
 
         return ResponseEntity.badRequest().body(ErrorResponseDTO.error("Request Error", errors));
     }
+    
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorResponseDTO<Object>> handleConstraintViolationException(ConstraintViolationException ex) {
